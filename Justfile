be4021b31fb6fbbc673e31ef203d1417c9f606dd
toyboot4e <toyboot4e@gmail.com>
2025-06-07 00:45:45 +0900

    wip

--- Justfile
+++ Justfile
@@ -6,15 +6,18 @@ help:
     @just -l
 
 # runs `plover-drills`
-run:
-    uv run src/main.py
+[positional-arguments]
+run *args:
+    uv run src/main.py "$@"
 
 [private]
 alias r := run
 
 # runs check with ruff
 check:
+    uvx ruff format src/*.py
     uvx ruff check --fix src/*.py
+    uvx pyright
 
 [private]
 alias c := check
@@ -23,6 +26,7 @@ alias chk := check
 # runs format with ruff
 format:
     uvx ruff format src/*.py
+    uvx ruff check --fix src/*.py
 
 [private]
 alias f := format
--- src/main.py
+++ src/main.py
@@ -1,30 +1,68 @@
+import sys
 from pathlib import Path
+from typing import Optional
 
 import plover
 from plover import system
-
 from plover.dictionary.base import load_dictionary
 from plover.dictionary.json_dict import JsonDictionary
 from plover.registry import registry
+from plover.steno import Stroke
+from plover.steno_dictionary import StenoDictionary
 
+# TODO: Update only required cmponents?
 registry.update()
 
-# TODO: setup other system?
+# TODO: What is the 'system'?
 system.setup("English Stenotype")
 
+# TODO: Use some logger.
 
-def demo_lookup(dict_path: Path):
-    print(f"loading dictionary at `{dict_path}`")
+
+# Text created from steno outline.
+type Translation = str
+
+type StrokeText = str
+
+# A series of strokes performed in succession.
+# https://plover.readthedocs.io/en/latest/api/steno.html#steno-notation
+type Outline = tuple[StrokeText, ...]
+
+
+# See: https://plover.readthedocs.io/en/latest/dict_formats.html
+def select_outline(dict: StenoDictionary, target_translation: str) -> Optional[Outline]:
+    "Selects the most reasonable steno outline for producing the translation."
+    stroke_outlines: list[Outline] = dict.reverse_lookup(target_translation)
+
+    if len(stroke_outlines) == 0:
+        print("found no steno outline for the input")
+        return
+
+    outline: Outline = next(iter(stroke_outlines))
+    return outline
+
+
+def run_lookup(dict_path: Path, target_translation: str):
     dict: JsonDictionary = load_dictionary(str(dict_path))
-    print(dict)
-    print(dict.reverse_lookup("cat"))
-    # => {('KAT',)}
+
+    outline: Optional[Outline] = select_outline(dict, target_translation)
+    if outline is None:
+        print("cannot find outline")
+        return
+
+    for stroke_str in outline:
+        stroke: Stroke = Stroke.from_steno(stroke_str)
+        print(stroke.steno_keys)
+
+
+def default_dict_path() -> Path:
+    return Path.home() / ".config/plover/main.json"
 
 
 def main():
     print(f"Plover version: {plover.__version__}")
-    dict_path = Path.home() / ".config/plover/main.json"
-    demo_lookup(dict_path)
+    dict_path: Path = default_dict_path()
+    run_lookup(dict_path, sys.argv[1])
 
 
 if __name__ == "__main__":
