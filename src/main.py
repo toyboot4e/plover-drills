9653bd1819332c656a2573e8850f8d98fe36a442
toyboot4e <toyboot4e@gmail.com>
2025-06-06 22:42:00 +0900

    wip not working

--- src/main.py
+++ src/main.py
@@ -1,10 +1,21 @@
+from pathlib import Path
+
 import plover
+
+from plover.dictionary.base import load_dictionary
+
+
+def demo_lookup(dict_path: Path):
+    print(f"loading dictionary at `{dict_path}`")
+    dictionary = load_dictionary(str(dict_path))
+    print(dictionary)
 
 
 def main():
     print(f"Plover version: {plover.__version__}")
+    dict_path = Path.home() / ".config/plover/main.json"
+    demo_lookup(dict_path)
 
 
 if __name__ == "__main__":
     main()
-
