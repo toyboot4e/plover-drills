5b349e7a84487289d8fa6e6cba8b6a5c98438a51
toyboot4e <toyboot4e@gmail.com>
2025-06-15 19:52:23 +0900

    introduce `loguru`

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from pathlib import Path
 from typing import Optional
 
 import plover
+from loguru import logger
 from plover import system
 from plover.config import Config, DictionaryConfig
 from plover.dictionary.base import load_dictionary
@@ -17,9 +18,6 @@ from plover.steno_dictionary import StenoDictionary, StenoDictionaryCollection
 import drill.layout as layout
 from drill.types import Outline, StrokeText, Translation
 
-# TODO: Use some logger.
-
-
 # TODO: use Suggestion
 def select_outline(dict: StenoDictionary, target_translation: str) -> Optional[Outline]:
     """Selects the most reasonable steno outline for producing the translation.
@@ -29,7 +27,6 @@ def select_outline(dict: StenoDictionary, target_translation: str) -> Optional[O
     stroke_outlines: list[Outline] = dict.reverse_lookup(target_translation)
 
     if len(stroke_outlines) == 0:
-        print("found no steno outline for the input")
         return
 
     outline: Outline = next(iter(stroke_outlines))
@@ -39,6 +36,7 @@ def select_outline(dict: StenoDictionary, target_translation: str) -> Optional[O
 def run_lookup(dict: StenoDictionaryCollection, target_translation: str):
     outline: Optional[Outline] = select_outline(dict, target_translation)
     if outline is None:
+        logger.error("found no steno outline for the input")
         return
 
     outline_strings: list[list[str]] = map(
@@ -53,7 +51,6 @@ def run_lookup(dict: StenoDictionaryCollection, target_translation: str):
 
 def run_lesson(config: Config, system_name: str):
     # Load the default system defined in user configuration
-    # print(f"system name: {config["system_name"]}")
 
     # Setup system
     system.setup(system_name)
@@ -62,9 +59,9 @@ def run_lesson(config: Config, system_name: str):
     enabled_dictionaries: list[DictionaryConfig] = [
         dict_config for dict_config in config["dictionaries"] if dict_config.enabled
     ]
-    print(f"enabled dictionaries: {enabled_dictionaries}")
+    logger.info(f"enabled dictionaries: {enabled_dictionaries}")
 
-    # FIXME: It shows errors?
+    # FIXME: suppress error logs here
     dicts: list[StenoDictionary] = list(
         map(
             lambda d: load_dictionary(d.path),
@@ -77,41 +74,25 @@ def run_lesson(config: Config, system_name: str):
     run_lookup(dict, "beginner")
 
 
-# FIXME: be ready with plugins installed with `plover_flake`
-def not_main():
-    # FIXME: It does not detect plugins installed with `plover-flake`
+def main():
     registry.update()
 
-    # TODO: get available system names
+    # Get available system names
     system_plugins = registry.list_plugins("system")
     system_names = [plugin.name for plugin in system_plugins]
-    print(f"available systems: {system_names}")
+    logger.info(f"available systems: {system_names}")
 
     # Load the configuration
-    print(f"config path: {CONFIG_FILE}")
+    logger.info(f"config path: {CONFIG_FILE}")
     config = Config(CONFIG_FILE)
     config.load()
 
-    # print(config.as_dict())
+    logger.trace(config.as_dict())
 
     system_name: str = config["system_name"]
-    print(f"system name: {system_name}")
+    logger.info(f"system name: {system_name}")
     run_lesson(config, system_name)
 
 
-def main():
-    """Tests English Stenoword"""
-    registry.update()
-
-    # FIXME: plover-flake registry
-    system.setup("English Stenotype")
-    dict = load_dictionary(str(Path(CONFIG_DIR) / "main.json"))
-    dict = StenoDictionaryCollection([dict])
-
-    # run
-    run_lookup(dict, sys.argv[1])
-
-
 if __name__ == "__main__":
-    # main()
-    not_main()
+    main()
