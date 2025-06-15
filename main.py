4e74729472a964ae3001dcfc471f40b38dd5b1db
toyboot4e <toyboot4e@gmail.com>
2025-06-15 20:28:11 +0900

    show all in a typey type lesson file

--- main.py
+++ main.py
@@ -14,15 +14,18 @@ from plover.oslayer.config import CONFIG_DIR, CONFIG_FILE
 from plover.registry import registry
 from plover.steno import Stroke
 from plover.steno_dictionary import StenoDictionary, StenoDictionaryCollection
+from typing_extensions import Self
 
 import drill.layout as layout
 from drill.types import Outline, StrokeText, Translation
 
-# TODO: use Suggestion
+
 def select_outline(dict: StenoDictionary, target_translation: str) -> Optional[Outline]:
     """Selects the most reasonable steno outline for producing the translation.
 
     See: https://plover.readthedocs.io/en/latest/dict_formats.html
+
+    TODO: use Suggestion
     """
     stroke_outlines: list[Outline] = dict.reverse_lookup(target_translation)
 
@@ -33,45 +36,90 @@ def select_outline(dict: StenoDictionary, target_translation: str) -> Optional[O
     return outline
 
 
+def join_colored_outline_strokes(outline: list[list[str]]) -> str:
+    outline_strings: list[list[str]] = map(
+        lambda s: layout.show_colored_stroke(Stroke.from_steno(s)), outline
+    )
+    return ["    ".join(rows) for rows in zip(*outline_strings)]
+
+
+def print_colored_outline(outline: list[list[str]]) -> str:
+    for row in join_colored_outline_strokes(outline):
+        print(row)
+
+
 def run_lookup(dict: StenoDictionaryCollection, target_translation: str):
     outline: Optional[Outline] = select_outline(dict, target_translation)
     if outline is None:
         logger.error("found no steno outline for the input")
         return
 
-    outline_strings: list[list[str]] = map(
-        lambda s: layout.show_colored_stroke(Stroke.from_steno(s)), outline
-    )
-
     print(outline)
-    concatenated_rows = ["    ".join(rows) for rows in zip(*outline_strings)]
-    for row in concatenated_rows:
-        print(row)
+    print_colored_outline(outline)
 
 
-def run_lesson(config: Config, system_name: str):
-    # Load the default system defined in user configuration
+class Runner:
+    def __init__(self, config: Config, system_name: str, dict: StenoDictionaryCollection):
+        self.config = config
+        self.system_name = system_name
+        self.dict = dict
 
-    # Setup system
-    system.setup(system_name)
+    @classmethod
+    def from_config(cls, config: Config, system_name: str) -> Self:
+        # Setup system
+        system.setup(system_name)
 
-    # Load dictionaries
-    enabled_dictionaries: list[DictionaryConfig] = [
-        dict_config for dict_config in config["dictionaries"] if dict_config.enabled
-    ]
-    logger.info(f"enabled dictionaries: {enabled_dictionaries}")
+        # Load dictionaries
+        enabled_dictionaries: list[DictionaryConfig] = [
+            dict_config for dict_config in config["dictionaries"] if dict_config.enabled
+        ]
+        logger.info(f"enabled dictionaries: {enabled_dictionaries}")
 
-    # FIXME: suppress error logs here
-    dicts: list[StenoDictionary] = list(
-        map(
-            lambda d: load_dictionary(d.path),
-            enabled_dictionaries,
+        # FIXME: suppress error logs here
+        dicts: list[StenoDictionary] = list(
+            map(
+                lambda d: load_dictionary(d.path),
+                enabled_dictionaries,
+            )
         )
-    )
-    dict: StenoDictionaryCollection = StenoDictionaryCollection(dicts)
+        dict: StenoDictionaryCollection = StenoDictionaryCollection(dicts)
+        # FIXME: suppress error logs here
+        dicts: list[StenoDictionary] = list(
+            map(
+                lambda d: load_dictionary(d.path),
+                enabled_dictionaries,
+            )
+        )
+        dict: StenoDictionaryCollection = StenoDictionaryCollection(dicts)
+        return Runner(config, system_name, dict)
 
-    # TODO: run
-    run_lookup(dict, "beginner")
+
+class Lesson:
+    def __init__(self, data: list[tuple[str, str]]):
+        self.data = data
+
+    @classmethod
+    def load_typey_type(cls, path: Path):
+        def is_comment(line: str) -> bool:
+            return line.strip() == ""
+
+        data: list[tuple[str, str]] = []
+        # TODO: handle exception
+        with path.open(mode="r") as f:
+            for line in f:
+                if not is_comment(line):
+                    word, outline = line.split("\t")
+                    data.append([word.strip(), outline.strip()])
+        return Lesson(data)
+
+
+def run_lesson(runner: Runner, lesson: Lesson):
+    # Load the default system defined in user configuration
+    print(lesson.data)
+    for word, outline in lesson.data:
+        print(word)
+        print_colored_outline(outline)
+        print("")
 
 
 def main():
@@ -82,16 +130,24 @@ def main():
     system_names = [plugin.name for plugin in system_plugins]
     logger.info(f"available systems: {system_names}")
 
-    # Load the configuration
+    # Config
     logger.info(f"config path: {CONFIG_FILE}")
     config = Config(CONFIG_FILE)
     config.load()
-
     logger.trace(config.as_dict())
 
+    # System
     system_name: str = config["system_name"]
     logger.info(f"system name: {system_name}")
-    run_lesson(config, system_name)
+
+    # TODO: TUI for selecting lesson file
+    path = Path("./lessons/practice/5-EU.txt")
+    logger.trace(f"lesson path: {path}")
+    lesson = Lesson.load_typey_type(path)
+
+    # Run lesson
+    runner = Runner.from_config(config, system_name)
+    run_lesson(runner, lesson)
 
 
 if __name__ == "__main__":
