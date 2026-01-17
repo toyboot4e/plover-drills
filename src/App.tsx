5e877b0714a0dc9723309902085be047b6a639c8
toyboot4e <toyboot4e@gmail.com>
2026-01-18 05:30:06 +0900

    fix: draw dummy elements

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 import styles from './App.module.scss';
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       <h1>Plove Drills for Lapwing Theory</h1>
-      <textarea></textarea>
+      <p>Select lesson: [lesson selector here]</p>
+      <p>[1/n] Type this: example [accent hint here]</p>
       <p>
-        <div className={styles.editor} contentEditable placeholder='Type here'></div>
+        <div className={styles.editor} contenteditable />
       </p>
+      <p>Show outline here on type error</p>
       <footer className={styles.footer}>
         This is a third-party app for <a href='https://lapwing.aerick.ca/'>Lapwing for Beginners</a>. Every lesson data
         comes from the book.
