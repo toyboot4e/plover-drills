0c7cfff0b951c57018eaa187edd8fed6f99511b6
toyboot4e <toyboot4e@gmail.com>
2026-01-18 23:12:54 +0900

    fix: setup wrong `flake.nix` for X11 Linux only

--- gen/flake.nix
+++ gen/flake.nix
@@ -18,21 +18,53 @@
       in
       {
         devShells.default =
-          pkgs.mkShell {
-            buildInputs = with pkgs; [
-              pkg-config
-            ] ++ pkgs.lib.optional pkgs.stdenv.isLinux [
-              linuxHeaders
-	    ];
+          pkgs.mkShell rec {
+            nativeBuildInputs = with pkgs; [
+              zstd.dev
+              glib.dev
+              libGL
+              fontconfig
+              xorg.libX11
+              libxkbcommon
+              freetype
+              dbus.lib
+            ];
+
+            buildInputs =
+              with pkgs;
+              [
+                pkg-config
+                # qt6.qtbase
+                # qt6.wrapQtAppsHook
+                # qt6.qtsvg # required for rendering icons
+              ]
+              ++ pkgs.lib.optional pkgs.stdenv.isLinux [
+                linuxHeaders
+                # qt6.qtwayland
+              ];
 
             packages = with pkgs; [
               python3
               python3Packages.uv
               python3Packages.hid
             ];
-	  } // pkgs.lib.mkIf pkgs.stdenv.isLinux {
+
+            # FIXME: libgcc is better?
+            shellHook = ''
+              export LD_LIBRARY_PATH="${pkgs.stdenv.cc.cc.lib.outPath}/lib:$LD_LIBRARY_PATH"
+              export LD_LIBRARY_PATH="${pkgs.zstd.dev.out}/lib:$LD_LIBRARY_PATH"
+              export LD_LIBRARY_PATH="${pkgs.glib.dev.out}/lib:$LD_LIBRARY_PATH"
+              export LD_LIBRARY_PATH="${pkgs.libGL}/lib:$LD_LIBRARY_PATH"
+              export LD_LIBRARY_PATH="${pkgs.fontconfig.lib}/lib:$LD_LIBRARY_PATH"
+              export LD_LIBRARY_PATH="${pkgs.xorg.libX11}/lib:$LD_LIBRARY_PATH"
+              export LD_LIBRARY_PATH="${pkgs.libxkbcommon}/lib:$LD_LIBRARY_PATH"
+              export LD_LIBRARY_PATH="${pkgs.freetype}/lib:$LD_LIBRARY_PATH"
+              export LD_LIBRARY_PATH="${pkgs.dbus.lib}/lib:$LD_LIBRARY_PATH"
+            '';
+          }
+          // pkgs.lib.mkIf pkgs.stdenv.isLinux {
             C_INCLUDE_PATH = "${pkgs.linuxHeaders}/include";
-	  };
+          };
       }
     );
 }
