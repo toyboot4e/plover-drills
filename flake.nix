b08bab3d5ac5a292c921010ad87f9da959fbc1a4
toyboot4e <toyboot4e@gmail.com>
2026-07-20 04:10:10 +0900

    build: nixify

--- flake.nix
+++ flake.nix
@@ -13,6 +13,56 @@
       pkgsFor = system: import nixpkgs { inherit system; };
     in
     {
+      packages = forAllSystems (
+        system:
+        let
+          pkgs = pkgsFor system;
+        in
+        {
+          default = pkgs.stdenv.mkDerivation (finalAttrs: {
+            pname = "plover-drills";
+            version = "0.0.0";
+            src = ./.;
+
+            pnpmDeps = pkgs.fetchPnpmDeps {
+              inherit (finalAttrs) pname version src;
+              fetcherVersion = 3;
+              hash = "sha256-zbDUbYdh0Gb1/7OQpuUM/DipbgP5MYrBu2kY8MUOySM=";
+            };
+
+            nativeBuildInputs = with pkgs; [
+              nodejs_24
+              pnpm
+              pnpmConfigHook
+              patchelf
+            ];
+
+            # `sass-embedded` ships a prebuilt `dart` binary whose ELF interpreter
+            # (and libstdc++) are absent in the sandbox; point it at Nix's.
+            preBuild = ''
+              for dart in $(find node_modules/.pnpm -path '*/sass-embedded-linux-x64/dart-sass/src/dart'); do
+                patchelf \
+                  --set-interpreter "$(cat "$NIX_CC/nix-support/dynamic-linker")" \
+                  --set-rpath "${lib.makeLibraryPath [ pkgs.stdenv.cc.cc.lib ]}" \
+                  "$dart"
+              done
+            '';
+
+            buildPhase = ''
+              runHook preBuild
+              pnpm run build
+              runHook postBuild
+            '';
+
+            installPhase = ''
+              runHook preInstall
+              cp -r dist $out
+              runHook postInstall
+            '';
+          });
+        }
+      );
+
       devShells = forAllSystems (
         system:
         let
@@ -22,6 +72,7 @@
           default = pkgs.mkShell {
             packages = with pkgs; [
               nodejs_24
+              pnpm
               just
               zizmor
             ];
